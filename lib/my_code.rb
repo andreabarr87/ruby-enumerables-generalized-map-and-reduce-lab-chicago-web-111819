34b9fe22c3ea1254c9cc35ce9f6d2b626a1b9ea7
Andrea S Barr <andreabarr87@gmail.com>
2019-11-13 04:18:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,17 +10,17 @@ end
 
 
 
-def reduce(array, start = nil)
-  if start
-    total = start 
-    counter = 0
-  else 
-    total = array[0]
-  end
-  while counter < array.length do
-    total = yield(total, array[counter])
-    counter += 1
-    total
+#def reduce(array, start = nil)
+#  if start
+ #   total = start 
+  #  counter = 0
+#  else 
+ #   total = array[0]
+#  end
+ # while counter < array.length do
+  #  total = yield(total, array[counter])
+   # counter += 1
+    #total
   end
   total
 end
